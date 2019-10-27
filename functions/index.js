6d5152b32608e166a7ad652fdc5f825743496ed0
服部一樹 <kazuki@hattorikazukinoMacBook-Pro.local>
2019-10-27 18:36:54 +0900

    Please entect methodr the commit message for your changes. Lines starting

--- functions/index.js
+++ functions/index.js
@@ -6,20 +6,26 @@ const docRef = admin
   .collection(functions.config().firestore.collection)
   .doc(functions.config().firestore.document)
 
+const GOOD = 1000.0
+const NORMAL = 500.0
+const BAD = 300.0
+const getStatus = function(soilMoistureSensor) {
+  if (soilMoistureSensor >= GOOD) {
+    return 0
+  } else if (soilMoistureSensor > NORMAL && soilMoistureSensor < GOOD) {
+    return 3
+  } else {
+    return 1
+  }
+}
+
 exports.addRecord = functions.database
   .ref(functions.config().database.id)
   .onUpdate((change, context) => {
     const plantData = change.after._data
     let transaction = admin.firestore().runTransaction(t => {
       let soilMoistureSensor = plantData[functions.config().database.key]
-      var status = 1
-      if (soilMoistureSensor > 100) {
-        status = 0
-      } else if (soilMoistureSensor > 30 && soilMoistureSensor < 100) {
-        status = 1
-      } else {
-        status = 2
-      }
+      let status = getStatus(soilMoistureSensor)
       return t
         .get(docRef)
         .then(doc => {
